e43d8a4c15b382393e89aa26c55c31a4cfcb9ad8
Will Barkoff <william@barkoffusa.com>
2023-05-11 16:56:01 -0400

    add confetti

--- static/index.js
+++ static/index.js
@@ -28,6 +28,7 @@ const destTime = convertFromGoDate(countdownData.countdownTo)
 
 const observationUrl = "https://api.weather.gov/stations/KITH/observations/latest"
 
+
 const getTemp = data => {
 	const value = data.properties.temperature.value
 	const unit = data.properties.temperature.unitCode
@@ -81,7 +82,7 @@ const tick = () => {
 
 if (countdownData.showMessage && window.countdownInfo.message.header != "bracket") {
 	infoTitle.textContent = countdownData.message.header
-	if (countdownData.message.body) {
+	if (countdownData.message.body && countdownData.message.body != "__confetti__") {
 		infoSubtitle.textContent = countdownData.message.body
 	} else {
 		infoSubtitle.classList.add("hidden")
@@ -132,4 +133,113 @@ if (window.countdownInfo.message.header == "bracket") {
 	})
 
 	madnessContainer.classList.remove("hidden")
+}
+
+if (countdownData.showMessage && window.countdownInfo.message.body == "__confetti__") {
+	console.log("loading confetti")
+
+	tsParticles.load("tsparticles", {
+		"fullScreen": {
+			"zIndex": 1
+		},
+		"particles": {
+			"color": {
+				"value": [
+					"#FFFFFF",
+					"#FFd700"
+				]
+			},
+			"move": {
+				"direction": "bottom",
+				"enable": true,
+				"outModes": {
+					"default": "out"
+				},
+				"size": true,
+				"speed": {
+					"min": 1,
+					"max": 3
+				}
+			},
+			"number": {
+				"value": 500,
+				"density": {
+					"enable": true,
+					"area": 800
+				}
+			},
+			"opacity": {
+				"value": 1,
+				"animation": {
+					"enable": false,
+					"startValue": "max",
+					"destroy": "min",
+					"speed": 0.3,
+					"sync": true
+				}
+			},
+			"rotate": {
+				"value": {
+					"min": 0,
+					"max": 360
+				},
+				"direction": "random",
+				"move": true,
+				"animation": {
+					"enable": true,
+					"speed": 60
+				}
+			},
+			"tilt": {
+				"direction": "random",
+				"enable": true,
+				"move": true,
+				"value": {
+					"min": 0,
+					"max": 360
+				},
+				"animation": {
+					"enable": true,
+					"speed": 60
+				}
+			},
+			"shape": {
+				"type": [
+					"circle",
+					"square"
+				],
+				"options": {}
+			},
+			"size": {
+				"value": {
+					"min": 2,
+					"max": 4
+				}
+			},
+			"roll": {
+				"darken": {
+					"enable": true,
+					"value": 30
+				},
+				"enlighten": {
+					"enable": true,
+					"value": 30
+				},
+				"enable": true,
+				"speed": {
+					"min": 15,
+					"max": 25
+				}
+			},
+			"wobble": {
+				"distance": 30,
+				"enable": true,
+				"move": true,
+				"speed": {
+					"min": -15,
+					"max": 15
+				}
+			}
+		}
+	});
 }
